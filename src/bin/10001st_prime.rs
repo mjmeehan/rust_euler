595f3e223971e8a7a07170631bd4ea294e5d43b4
Mike Meehan <mjmeehan@gmail.com>
2016-11-04 07:21:22 -0400

    finished 10001st_prime

---
+++ src/bin/10001st_prime.rs
@@ -0,0 +1,30 @@
+/**
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is
+ * 13. What is the 10 001st prime number?
+ **/
+extern crate euler;
+use std::vec;
+
+fn is_prime(n: i64, cache: &Vec<i64>) -> bool
+{
+    for prime in cache {
+        if n % prime == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn main()
+{
+    let mut cache = vec![2, 3, 5, 7, 11, 13];
+    let mut i = 14;
+    while cache.len() < 10002 {
+        if is_prime(i, &cache) {
+            print!("found prime {}\n", i);
+            cache.push(i);
+        }
+        i += 1;
+    }
+    print!{"{}\n", cache[10001]};
+}
