036766b02edf646350f055d0a55111b22bd2f88e
Mike Meehan <mjmeehan@gmail.com>
2019-01-09 08:11:31 -0500

    longest_collatz_sequence: done

--- src/bin/longest_collatz_sequence.rs
+++ src/bin/longest_collatz_sequence.rs
@@ -17,5 +17,78 @@ Which starting number, under one million, produces the longest chain?
 NOTE: Once the chain starts the terms are allowed to go above one million.
 
 **/
+use std::collections::btree_map::BTreeMap;
 
-fn main() {}
+type CollatzNum = u64;
+type CollatzResult = (CollatzNum, usize);
+type CollatzCache = BTreeMap<CollatzNum, CollatzResult>;
+
+
+fn collatz(x: CollatzNum) -> CollatzNum {
+    if x == 1 {
+        1
+    } else if x % 2 == 0 {
+        // even
+        x / 2
+    } else {
+        // odd
+        3 * x + 1
+    }
+}
+
+fn collatz_chain(start: CollatzNum, cache: &mut CollatzCache) -> CollatzResult
+{
+    if let Some(&(end, depth)) = cache.get(&start) { // in cache?
+        return (end, depth);
+    }
+    let mut prev = start.clone();
+    let mut chain = Vec::new();
+    loop {
+        //print!(" {}", prev);
+        // base case
+        if prev == 1 {
+            let result = (1, 0);
+            println!("...base case, caching ({}, {:?})", prev, result);
+            cache.insert(prev, result);
+        } else {
+            chain.push(prev);
+        }
+        // In cache
+        if let Some(&(end, depth)) = cache.get(&prev) {
+            let step = chain.len();
+            //println!("...in cache, {} more", depth);
+            // unwind chain, add to cache incrementing depth
+            cache_chain(cache, &mut chain, depth, end);
+            //println!();
+            return (end, depth + step);
+        }
+        let next = collatz(prev);
+        //print!(" -> ");
+        prev = next;
+    }
+}
+
+fn cache_chain(cache: &mut CollatzCache, chain: &mut Vec<CollatzNum>, depth: usize, prev_link: CollatzNum) {
+    //print!("Caching chaendin: ");
+    let mut prev_link = prev_link;
+    for (ordinal, &link) in chain.iter().rev().enumerate(){
+        let result = (prev_link, depth + ordinal);
+        //print!("({}, {:?}), ", link, result);
+        cache.insert(link, result);
+        prev_link = link;
+    }
+}
+
+fn main() {
+    let mut cache = CollatzCache::new();
+    //for start in 1..1_000_000 {
+    let mut longest = (1, 0);
+    for start in 1..1_000_001 {
+        let (next, depth) = collatz_chain(start, &mut cache);
+        if depth > longest.1 {
+            println!("New longest chain: start={}, next={}, depth={}", start, next, depth);
+            longest = (next, depth);
+        }
+
+    }
+}
